845a0c759c2cb88b73fe7bcaeba91ffa8e4b0a06
Jeremy Avigad <avigad@cmu.edu>
2025-05-15 17:20:23 -0400

    add example

--- EquationalTest/Basic.lean
+++ EquationalTest/Basic.lean
@@ -1,1 +1,37 @@
-def hello := "world"
+import Mathlib.Tactic
+import Duper
+
+class Magma (α : Type _) where
+  op : α → α → α
+
+infix:65 " ⋄ " => Magma.op
+
+abbrev Equation1689 (M : Type _) [Magma M] := ∀ x y z : M, x = (y ⋄ x) ⋄ ((x ⋄ z) ⋄ z)
+
+abbrev Equation2 (M : Type _) [Magma M] := ∀ x y : M, x = y
+
+variable {M : Type _} [Magma M]
+
+abbrev S (z x : M) := (x ⋄ z) ⋄ z
+
+abbrev f (x y : M) := x ⋄ (S y x)
+
+lemma f_eq (x y : M) : f x y = x ⋄ ((x ⋄ y) ⋄ y) := rfl
+
+lemma main_eq (h : Equation1689 M) (x y z : M) : x = (y ⋄ x) ⋄ S z x := by
+  duper [*]
+
+lemma claim (h : Equation1689 M) (a b : M) (y : M) (hy : ∃ z, y = z ⋄ a) : y ⋄ S b a = a := by
+  duper [*]
+
+lemma Lemma1 (h : Equation1689 M) (a b c : M) : S b a = a ⋄ (S c b) := by
+  duper [*]
+
+lemma Lemma2 (h : Equation1689 M) (a : M) : ∃ b c d, f b c = S d a := by
+  duper [*]
+
+lemma Lemma3 (h : Equation1689 M) (a : M) : ∃ e, S e a = a := by
+  duper [*]
+
+theorem singleton_law (h : Equation1689 M) : Equation2 M := by
+  duper [*]
